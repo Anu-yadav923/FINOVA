5c49fdbb9361e1057a8e1595f6b2d9eb0e91175c
Anu-yadav923 <anuy8306@gmail.com>
2026-08-16 22:02:03 +0530

    added repository

---
+++ Backend/src/services/user.service.js
@@ -0,0 +1,16 @@
+const {findUserById} = require("../repository/user.repository");
+
+const getUserById = async(userId) =>{
+
+    const user = findUserById(userId);
+
+    if(!user){
+        return null;
+    }
+
+    return user;
+}
+
+module.exports = {
+    getUserById
+};
